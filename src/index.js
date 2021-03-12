c72a394e61237bbfdbef19ebe7d023a357e9b754
will13ano <samuel-1998@hotmail.com.br>
2021-03-12 19:50:33 -0300

    "mais uma vez tentando por online"

--- src/index.js
+++ src/index.js
@@ -1,11 +1,8 @@
 const express = require('express');
-const bodyParser = require('body-parser');
+
 
 const app = express();
 const port = process.env.PORT || 3000;
-
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended : false }));
 
 app.get('/', (req, res) => {
     res.json({
