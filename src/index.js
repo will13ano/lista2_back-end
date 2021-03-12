a728da76567273ac67add79392fd574fea84c796
will13ano <samuel-1998@hotmail.com.br>
2021-03-12 19:47:52 -0300

    caminho para testar a api online

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ const port = process.env.PORT || 3000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended : false }));
 
-app.get('/chegou', (req, res) => {
+app.get('/', (req, res) => {
     res.json({
         message: "chegou"
     })
