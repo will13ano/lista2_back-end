78e3eaeaed91fa0c0dbcc521a2c80e95b425aad7
will13ano <samuel-1998@hotmail.com.br>
2021-03-12 20:11:52 -0300

    deus sou eu de novo

--- src/index.js
+++ src/index.js
@@ -7,7 +7,8 @@ const port = process.env.PORT || 3000;
 console.log({
     message: "chegou aqui",
     PORT: port
-})
+});
+
 require('./controller/projectController')(app);
 require('./controller/loginController')(app);
 require('./controller/listaController')(app);
