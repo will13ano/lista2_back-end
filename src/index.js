a5a1e2326287f600e99dbce6f7ae44aa659630f8
will13ano <samuel-1998@hotmail.com.br>
2021-03-12 20:00:54 -0300

    caminho para testar a api online

--- src/index.js
+++ src/index.js
@@ -4,11 +4,6 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-    res.json({
-        message: "chegou"
-    })
-});
 
 require('./controller/projectController')(app);
 require('./controller/loginController')(app);
